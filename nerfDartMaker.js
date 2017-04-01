18c732b3543a8dc6250b799599ed02e1f00da201
ahalekelly <ahalekelly@gmail.com>
2017-04-01 15:49:33 -0400

    Borders only

--- nerfDartMaker.js
+++ nerfDartMaker.js
@@ -4,15 +4,20 @@ tiles=[]
 
 var blue=13
 
-makeRectangle(56, 248, 56+50-8, 256, blue)
+makeRectangle(56, 248, 56+50-8, 256, blue, false)
 
-function makeRectangle(x1, y1, x2, y2, c) {
+function makeRectangle(x1, y1, x2, y2, c, fill) {
 	for (var x=x1; x<=x2; x++) {
-		for (var y=y1; y<=y2; y++) {
-			tiles.push([x,y,c])
+		if (fill) {
+			for (var y=y1; y<=y2; y++) {
+				tiles.push([x,y,c])
+			}
+		} else {
+			tiles.push([x,y1,c])
+			tiles.push([x,y2,c])
 		}
 	}
 }
 
 var output = "var tiles="+JSON.stringify(tiles)+";"
-fs.writeFile("nerfDart.js",output)
+fs.writeFileSync("nerfDart.js",output)
