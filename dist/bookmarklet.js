8e69afd0362bac179be8a66cdb1dcecfe83523d9
ahalekelly <ahalekelly@gmail.com>
2017-04-01 11:28:53 -0400

    Point to my repo, and inclue the js file

--- dist/bookmarklet.js
+++ dist/bookmarklet.js
@@ -1,6 +1,9 @@
 javascript:(function() {
+var j = document.createElement('script');
+j.setAttribute('type', 'text/javascript');
+j.setAttribute('src', 'https://raw.githubusercontent.com/ahalekelly/PlaceBot/master/blueCorner.js');
 var s = document.createElement('script');
 s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'https://grind086.github.io/PlaceBot/dist/placebot.min.js');
+s.setAttribute('src', 'https://raw.githubusercontent.com/ahalekelly/PlaceBot/master/placebot.js');
 document.head.appendChild(s);
 })();
