9d9586672365079852152a2f1b0eff61563e80e4
ahalekelly <ahalekelly@gmail.com>
2017-04-02 02:40:40 -0400

    Fixed white pixels

--- imageToArray.py
+++ imageToArray.py
@@ -10,19 +10,23 @@ palette = ["#FFFFFF", "#E4E4E4", "#888888", "#222222", "#FFA7D1", "#E50000", "#E
 print(image.mode)
 for x in range(image.width)[::-1]:
     for y in range(image.height):
-        color=False
+        color=-1
         pixel = image.getpixel((x,y))
         if pixel != (255,255,255,0):
             print(x,y,pixel)
         if pixel == (255,255,255,255):
+            print("white")
             color=0
         elif pixel == (0,0,234,255):
+            print("blue")
             color = 13
         elif pixel == (229, 149, 0,255):
+            print("orange")
             color = 6
-        if color:
+        if color != -1:
             tiles.append([x,y,color])
 print(tiles)
+print(len(tiles))
 output = "var tiles="+json.dumps(tiles)+";"
 with open('nerfDart.js', 'w') as outfile:
     outfile.write(output)
