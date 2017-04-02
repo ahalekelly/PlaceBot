fe489c2002ee24e264576e2b9a4c1d0ea9cbe9e1
ahalekelly <ahalekelly@gmail.com>
2017-04-02 08:39:50 -0400

    Larger text dart, and better png color detection

--- imageToArray.py
+++ imageToArray.py
@@ -1,30 +1,23 @@
 from PIL import Image, ImageDraw
 from glob import glob
 import json
-image = Image.open("nerfDartLeft.png")
+image = Image.open("nerfDart.png")
 
 tiles = []
 
-palette = ["#FFFFFF", "#E4E4E4", "#888888", "#222222", "#FFA7D1", "#E50000", "#E59500", "#A06A42", "#E5D900", "#94E044", "#02BE01", "#00D3DD", "#0083C7", "#0000EA", "#CF6EE4", "#820080"]
+palette = [(255,255,255,255), (228,228,228,255), (136,136,136,255), (34,34,34,255), (255,167,209,255), (229,0,0,255), (229,149,0,255), (160,106,66,255), (229,217,0,255), (148,224,68,255),
+           (2,190,1,255),(0,211,221,255), (0,131,199,255), (0,0,234,255), (207,110,228,255), (130,0,128,255)]
 
 print(image.mode)
-for x in range(image.width)[::-1]:
+for x in range(image.width):
     for y in range(image.height):
-        color=-1
         pixel = image.getpixel((x,y))
-        if pixel != (255,255,255,0):
+        try:
+            color = palette.index(pixel)
             print(x,y,pixel)
-        if pixel == (255,255,255,255):
-            print("white")
-            color=0
-        elif pixel == (0,0,234,255):
-            print("blue")
-            color = 13
-        elif pixel == (229, 149, 0,255):
-            print("orange")
-            color = 6
-        if color != -1:
             tiles.append([x,y,color])
+        except Exception:
+            pass
 print(tiles)
 print(len(tiles))
 output = "var tiles="+json.dumps(tiles)+";"
