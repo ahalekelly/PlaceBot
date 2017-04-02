c4a9b1d774fdcf5303619fa3ea0e4b5f8f101ee5
ahalekelly <ahalekelly@gmail.com>
2017-04-02 12:57:34 -0400

    Purging more files

--- import.js
+++
@@ -1,4 +0,0 @@
-var s = document.createElement('script');
-s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'https://grind086.github.io/PlaceBot/dist/placebot.min.js');
-document.head.appendChild(s);
