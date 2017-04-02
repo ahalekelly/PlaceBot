196babf9b35375c471a1e8d70bbfa79b8a590b93
ahalekelly <ahalekelly@gmail.com>
2017-04-02 12:55:49 -0400

    Removed old files

--- dist/bookmarklet.js
+++
@@ -1,9 +0,0 @@
-javascript:(function() {
-var j = document.createElement('script');
-j.setAttribute('type', 'text/javascript');
-j.setAttribute('src', 'https://raw.githubusercontent.com/ahalekelly/PlaceBot/master/blueCorner.js');
-var s = document.createElement('script');
-s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'https://raw.githubusercontent.com/ahalekelly/PlaceBot/master/placebot.js');
-document.head.appendChild(s);
-})();
